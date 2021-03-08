4c8f2bb9f9d6c41442ca9cfef912475978df9757
marcowong <wonghokchi0402@gmail.com>
2021-03-08 19:56:26 +0800

    Added LeapYearCalculator.java

---
+++ src/LeapYearCalculator.java
@@ -0,0 +1,17 @@
+public class LeapYearCalculator {
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    return year % 400 == 0;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+}
