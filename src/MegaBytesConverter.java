5faecc2cc70ca609e20db2a420c86c13c4a7920b
marcowong <wonghokchi0402@gmail.com>
2021-03-08 19:32:55 +0800

    Added MegaBytesConverter.java

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,10 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + (kiloBytes / 1024)
+                    + " MB and " + (kiloBytes % 1024) + " KB");
+        }
+    }
+}
