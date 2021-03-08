b9dba97029f74379d5836f901c3e775556d5b49d
marcowong <wonghokchi0402@gmail.com>
2021-03-08 19:46:18 +0800

    Added BarkingDog.java

---
+++ src/BarkingDog.java
@@ -0,0 +1,7 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking || hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else return hourOfDay < 8 || hourOfDay > 22;
+    }
+}
