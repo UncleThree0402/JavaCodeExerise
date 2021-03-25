3a753909a4e24a15aae4a2f0bbbe05310bd0ef2e
marcowong <wonghokchi0402@gmail.com>
2021-03-25 19:35:02 +0800

    Added Person.java

---
+++ src/Person.java
@@ -0,0 +1,49 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age <= 100) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+    }
+
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        } else if (firstName.isEmpty()) {
+            return lastName;
+        } else if (lastName.isEmpty()) {
+            return firstName;
+        } else {
+            return firstName + " " + lastName;
+        }
+    }
+}
